0c43f2dfac62d9fc32930f2e48fb4fef6c70f117
EricD <EricD@NI-24-NTB>
2020-06-04 13:55:18 -0400

    heroku

--- areas/home/home.area.ts
+++ areas/home/home.area.ts
@@ -2,7 +2,6 @@ import { Area } from "https://deno.land/x/alosaur/mod.ts";
 import { HomeController } from "./home.controller.ts";
 
 @Area({
-  baseRoute: "/",
   controllers: [HomeController],
 })
 export class HomeArea {}
